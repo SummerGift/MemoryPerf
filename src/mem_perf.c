9faf91f3bcad3a324b74aa6ae7a6cca22327ea0d
SummerGift <summergift2019@gmail.com>
2020-11-28 09:25:19 +0800

    [update] optimize cmd line for msh

--- src/mem_perf.c
+++ src/mem_perf.c
@@ -6,16 +6,15 @@
  * Change Logs:
  * Date           Author       Notes
  * 2020-11-27     SummerGift   first version
+ * 2020-11-28     SummerGift   optimize cmd
  */
 
 #include <stdio.h>
 #include "portme.h"
 
 #define ITERATIONS     200
-#define MEM_ADDR       (void *)0x10100000
-#define MEM_LEN        0x100000
 
-static uint32_t data_len = MEM_LEN * ITERATIONS;
+static uint32_t data_len;
 
 static void memory_perf_8bit(void *addr, uint32_t len)
 {
@@ -32,14 +31,14 @@ static void memory_perf_8bit(void *addr, uint32_t len)
         for(int i = 0; i < len; i+=8)
         {
             read_addr = (uint8_t *) (addr + i);
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
         }
     }
     stop_time_mp();
@@ -89,7 +88,6 @@ static void memory_perf_16bit(void *addr, uint32_t len)
 {
     volatile uint16_t *read_addr;
     float seconds, speed;
-    uint32_t data_len = 0x100000 * ITERATIONS;
 
     printf("--------------------------------------\n");
     printf("16-bit write speed test begin.\n");
@@ -100,14 +98,14 @@ static void memory_perf_16bit(void *addr, uint32_t len)
         for(int i = 0; i < len; i += 16)
         {
             read_addr = (uint16_t *) (addr + i);
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
         }
     }
     stop_time_mp();
@@ -157,7 +155,6 @@ static void memory_perf_32bit(void *addr, uint32_t len)
 {
     volatile uint32_t *read_addr;
     float seconds, speed;
-    uint32_t data_len = 0x100000 * ITERATIONS;
 
     printf("--------------------------------------\n");
     printf("32-bit write speed test begin.\n");
@@ -168,14 +165,14 @@ static void memory_perf_32bit(void *addr, uint32_t len)
         for(int i = 0; i < len; i += 32)
         {
             read_addr = (uint32_t *) (addr + i);
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
-            *read_addr++ = 0x05;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
+            *read_addr++ = 0x55;
         }
     }
     stop_time_mp();
@@ -221,13 +218,21 @@ static void memory_perf_32bit(void *addr, uint32_t len)
     printf("32-bit Read speed: %f M/s.\n", speed / 1000000);
 }
 
-static int memory_perf(void)
+static void memory_perf(int argc, char** argv)
 {
-    void *address = MEM_ADDR;
-    uint32_t len = MEM_LEN;
+    if (argc != 3)
+    {
+        printf("Please set test address and length correctly.\n");
+        printf("Example: <memory_perf 0x10100000 0x100000>\n");
+        return;
+    }
+
+    void *address = (void *)strtol(argv[1] + 2, NULL, 16);
+    uint32_t len = (uint32_t)strtol(argv[2] + 2, NULL, 16);
+    data_len = ITERATIONS * len;
 
     printf("Memory performance testing start...\n");
-    printf("address: %p, length: 0x%lx\n", address, len);
+    printf("address: %p, length: 0x%lx, iterations: %d\n", address, len, ITERATIONS);
     printf("Data length : %lu MB.\n", data_len / 1000000);
 
     memory_perf_8bit(address, len);
@@ -235,6 +240,5 @@ static int memory_perf(void)
     memory_perf_32bit(address, len);
 
     printf("Memory performance completed.\n");
-    return 0;
 }
 MSH_CMD_EXPORT(memory_perf, run memory performance test);
